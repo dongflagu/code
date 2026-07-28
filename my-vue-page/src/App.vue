77f62368989afdf568de32ba561fd19af9208c85
dongflagu <551417590@qq.com>
2026-07-28 13:54:59 +0800

    axios

--- my-vue-page/src/App.vue
+++ my-vue-page/src/App.vue
@@ -6,4 +6,14 @@
 </template>
 
 <script setup>
+import axios from 'axios'
+async function loadLocalJson() {
+  try {
+    const result = await axios.get('/testData.json')
+    console.log('拿到的json数据:', result.data.data)
+  } catch (err) {
+    console.log('读取失败：', err)
+  }
+}
+loadLocalJson()
 </script>
